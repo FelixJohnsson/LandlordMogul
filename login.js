c09eb3b68a5e9dc5522b3f155394073859a006c6
EternalMarch <32583303+EternalMarch@users.noreply.github.com>
2019-07-29 22:41:16 +0200

    Cleaned repo

--- login.js
+++
@@ -1,6 +0,0 @@
-function openRegisterForm(){
-    document.getElementById('register-form').style.display = 'block';
-}
-function openLoginForm(){
-    document.getElementById('login-form').style.display = 'block';
-}
